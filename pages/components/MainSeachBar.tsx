3b91bd2cb7f0aeeaa240b7413a7539b46ae67b7b
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-07 22:44:41 +0100

    retrieve gameNames from api, take search width prop

--- pages/components/MainSeachBar.tsx
+++ pages/components/MainSeachBar.tsx
@@ -1,17 +1,25 @@
 import { Autocomplete, Button, Group, Select } from "@mantine/core";
 import { useEffect, useState } from "react";
 
-export default function MainSearchBar() {
+interface MainSeachBarProps {
+	searchBarWidth?: string
+}
+
+export default function MainSearchBar(props: MainSeachBarProps) {
 	const [searchType, setSearchType] = useState<string>('game');
-	const gameInfo: string[] = [];
+	const [gameNames, setGameNames] = useState<string[]>([]);
+
+	async function getGameNames() {
+		const names = await fetch('/api/gameNames')
+		setGameNames(await names.json());
+	}
 
 	useEffect(() => {
-		console.log(searchType)
-	}, [searchType])
-
+		getGameNames()
+	}, [])
 
 	return <Group>
-		<Autocomplete data={gameInfo} sx={{ flexGrow: 1}}/>
+		<Autocomplete data={gameNames} sx={{ flexGrow: 1, width: props.searchBarWidth}} limit={20} maxDropdownHeight={200}/>
 		<Select value={searchType} onChange={(value) => setSearchType(value ?? '')} sx={{width: '90px'}} data={[{ value: 'game', label: 'Game'}, { value: 'user', label: 'User'}]}/>
 		<Button>Search</Button>
 	</Group>
