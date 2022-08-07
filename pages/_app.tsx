3c5418232453609b531ceb29f8dcb30b623d2442
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-07 18:16:27 +0100

    add mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+	
+	function toggleColorScheme(value?: ColorScheme) {
+		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+	}
+
+	return (
+		<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+			<MantineProvider theme={{colorScheme}} withGlobalStyles withNormalizeCSS>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</ColorSchemeProvider>
+	)
 }
 
 export default MyApp
