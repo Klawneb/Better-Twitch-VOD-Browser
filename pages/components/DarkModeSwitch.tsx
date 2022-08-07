3fdb382bcd484a4445eff98e5ce4e9284d5e4d9f
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-07 22:44:06 +0100

    add DarkModeSwitch component

---
+++ pages/components/DarkModeSwitch.tsx
@@ -0,0 +1,12 @@
+import { Group, Switch, useMantineColorScheme } from "@mantine/core";
+import { Moon, Sun } from "tabler-icons-react";
+
+export default function DarkModeSwitch() {
+	const { colorScheme, toggleColorScheme} = useMantineColorScheme();
+
+	return <Group spacing={"xs"}>
+		<Sun/>
+		<Switch onChange={() => toggleColorScheme()}/>
+		<Moon/>
+	</Group>
+}
