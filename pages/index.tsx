93f3c7922fa1b9195ace30480e871eac7f4bf33d
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-08 17:49:52 +0100

    move components folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import { Center, Stack, Title, Text, Switch, useMantineColorScheme} from '@manti
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import DarkModeSwitch from './components/DarkModeSwitch'
-import MainSearchBar from './components/MainSeachBar'
+import DarkModeSwitch from '../components/DarkModeSwitch'
+import MainSearchBar from '../components/MainSeachBar'
 
 function Home() {
 	return (
