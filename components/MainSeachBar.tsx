043df45d16f270cef5718b63daea10a9ef978215
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-08 20:11:54 +0100

    add searchinput state and link button

--- components/MainSeachBar.tsx
+++ components/MainSeachBar.tsx
@@ -1,4 +1,5 @@
 import { Autocomplete, Button, Group, Select } from "@mantine/core";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 interface MainSeachBarProps {
@@ -7,6 +8,7 @@ interface MainSeachBarProps {
 
 export default function MainSearchBar(props: MainSeachBarProps) {
 	const [searchType, setSearchType] = useState<string>('game');
+	const [searchInput, setSearchInput] = useState<string>('');
 	const [gameNames, setGameNames] = useState<string[]>([]);
 
 	async function getGameNames() {
@@ -19,8 +21,10 @@ export default function MainSearchBar(props: MainSeachBarProps) {
 	}, [])
 
 	return <Group>
-		<Autocomplete data={gameNames} sx={{ flexGrow: 1, width: props.searchBarWidth}} limit={20} maxDropdownHeight={200}/>
+		<Autocomplete data={gameNames} value={searchInput} onChange={setSearchInput} sx={{ flexGrow: 1, width: props.searchBarWidth}} limit={20} maxDropdownHeight={200}/>
 		<Select value={searchType} onChange={(value) => setSearchType(value ?? '')} sx={{width: '90px'}} data={[{ value: 'game', label: 'Game'}, { value: 'user', label: 'User'}]}/>
-		<Button>Search</Button>
+		<Link href={`/${searchType}/${searchInput}`} passHref>
+			<Button component="a">Search</Button>
+		</Link>
 	</Group>
 }
