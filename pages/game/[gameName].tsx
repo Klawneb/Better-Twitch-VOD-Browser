3f5974847d892308a70cf49ff7d626ae54309e3e
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-08 22:42:48 +0100

    add game vod view page

--- pages/game/[gameName].tsx
+++ pages/game/[gameName].tsx
@@ -1,10 +1,30 @@
-import { Title } from "@mantine/core";
+import { AspectRatio, Group, Title, Image, Space } from "@mantine/core";
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
 import VodViewHeader from "../../components/VodViewHeader";
 
 export default function GameVodView() {
 	const router = useRouter();
+	const [boxArtURL, setBoxArtURL] = useState('');
 	const { gameName } = router.query;
+	
+	async function getBoxArtURL() {
+		const response = await fetch(`/api/boxArt/${gameName}/`);
+		setBoxArtURL(await response.json());
+	}
 
-	return <VodViewHeader/>
+	useEffect(() => {
+		if (router.isReady) {
+			getBoxArtURL()
+		}
+	})
+
+	return <div>
+		<VodViewHeader/>
+		<Space h={"md"}/>
+		<Group position="center" align={"self-end"}>
+			<Image width={200} height={266.67} src={boxArtURL} alt={"Box Art"}/>
+			<Title>{gameName}</Title>
+		</Group>
+	</div>
 }
