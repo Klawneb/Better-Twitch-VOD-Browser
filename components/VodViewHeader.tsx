f7b31947f363828a1d7c6e2de27f2ed001dc00c4
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-09 19:05:43 +0100

    improve navbar scaling on smaller screens

--- components/VodViewHeader.tsx
+++ components/VodViewHeader.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, Header, Title } from "@mantine/core";
+import { Button, Group, Header, MediaQuery, Title } from "@mantine/core";
 import Link from "next/link";
 import { ChevronLeft } from "tabler-icons-react";
 import DarkModeSwitch from "./DarkModeSwitch";
@@ -7,12 +7,20 @@ import MainSearchBar from "./MainSeachBar";
 export default function VodViewHeader() {
 
 	return <Header height={60}>
-		<Group align={'center'} position={'apart'} sx={{height: "60px", padding: "0 10px"}}>
-			<Link href="/">
-				<Button component="a" leftIcon={<ChevronLeft/>}>Back</Button>
-			</Link>
-			<MainSearchBar searchBarWidth="500px"/>
-			<DarkModeSwitch/>
-		</Group>
+		<MediaQuery largerThan={'sm'} styles={{display: "none"}}>
+			<Group align={'center'} position={'apart'} sx={{height: "60px", padding: "0 10px"}} noWrap>
+				<MainSearchBar width="100%"/>
+			</Group>
+		</MediaQuery>
+
+		<MediaQuery smallerThan={'sm'} styles={{display: "none"}}>
+			<Group align={'center'} position={'apart'} sx={{height: "60px", padding: "0 10px"}} noWrap>
+				<Link href="/">
+					<Button component="a" leftIcon={<ChevronLeft/>}>Back</Button>
+				</Link>
+				<MainSearchBar width="30%"/>
+				<DarkModeSwitch/>
+			</Group>
+		</MediaQuery>
 	</Header>
 }
