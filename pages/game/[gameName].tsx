5b72b2021ec3fb35000370e496a623078ea5ef49
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-09 20:03:46 +0100

    remove boxart endpoint

--- pages/game/[gameName].tsx
+++ pages/game/[gameName].tsx
@@ -2,29 +2,17 @@ import { AspectRatio, Group, Title, Image, Space } from "@mantine/core";
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 import VodViewHeader from "../../components/VodViewHeader";
+import { languageCodes } from "../../data/languageCodes";
 
 export default function GameVodView() {
 	const router = useRouter();
-	const [boxArtURL, setBoxArtURL] = useState('');
 	const { gameName } = router.query;
-	
-	async function getBoxArtURL() {
-		const response = await fetch(`/api/boxArt/${gameName}/`);
-		setBoxArtURL(await response.json());
-	}
 
-	useEffect(() => {
-		if (router.isReady) {
-			getBoxArtURL()
-		}
-	})
 
 	return <div>
-		<VodViewHeader/>
+		<VodViewHeader gameName={gameName as string}/>
 		<Space h={"md"}/>
 		<Group position="center" align={"self-end"}>
-			<Image width={200} height={266.67} src={boxArtURL} alt={"Box Art"}/>
-			<Title>{gameName}</Title>
 		</Group>
 	</div>
 }
