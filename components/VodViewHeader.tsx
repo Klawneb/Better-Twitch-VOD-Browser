ed12b60ce88108464c8c2faac740c99732adad90
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-09 21:13:32 +0100

    switch searchbar position in header

--- components/VodViewHeader.tsx
+++ components/VodViewHeader.tsx
@@ -30,13 +30,13 @@ export default function VodViewHeader(props: VodViewHeaderProps) {
 				<Link href="/">
 					<Button component="a" leftIcon={<ChevronLeft/>}>Back</Button>
 				</Link>
+				<MainSearchBar width="25%"/>
 				<Group noWrap>
 					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('language', value ?? '')} value={props.searchProperties.language} label='Language' data={languages} searchable/>
 					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('sortBy', value ?? '')} value={props.searchProperties.sortBy} label='Sort by' data={[{ value: 'time', label: 'Latest'}, { value: 'views', label: 'Views'}, { value: 'trending', label: 'Trending'}]}/>
 					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('period', value ?? '')} value={props.searchProperties.period} label='Period' data={[{ value: 'all', label: 'All'}, { value: 'day', label: 'Day'}, { value: 'week', label: 'Week'}, { value: 'month', label: 'Month'}]}/>
 					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('type', value ?? '')} value={props.searchProperties.type} label='VOD type' data={[{ value: 'all', label: 'All'}, { value: 'upload', label: 'Upload'}, { value: 'archive', label: 'Archive'}, { value: 'highlight', label: 'Highlight '}]}/>
 				</Group>
-				<MainSearchBar width="20%"/>
 				<DarkModeSwitch/>
 			</Group>
 		</MediaQuery>
