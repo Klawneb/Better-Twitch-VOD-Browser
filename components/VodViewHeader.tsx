face97c3c1e7af135217319a1d903aa52235d9f9
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-09 21:07:23 +0100

    add search option fields

--- components/VodViewHeader.tsx
+++ components/VodViewHeader.tsx
@@ -1,12 +1,24 @@
-import { Button, Group, Header, MediaQuery, Title } from "@mantine/core";
+import { Button, Group, Header, MediaQuery, Text, Image, Select } from "@mantine/core";
 import Link from "next/link";
 import { ChevronLeft } from "tabler-icons-react";
+import { languageCodes } from "../data/languageCodes";
 import DarkModeSwitch from "./DarkModeSwitch";
 import MainSearchBar from "./MainSeachBar";
 
-export default function VodViewHeader() {
+interface VodViewHeaderProps {
+	searchProperties: {
+		language: string,
+		sortBy: string,
+		period: string,
+		type: string
+	}
+	handleSearchPropertyChange(field: 'language' | 'sortBy' | 'period' | 'type', value: string): void
+}
 
-	return <Header height={60}>
+export default function VodViewHeader(props: VodViewHeaderProps) {
+	const languages = languageCodes;
+
+	return <Header height={70}>
 		<MediaQuery largerThan={'sm'} styles={{display: "none"}}>
 			<Group align={'center'} position={'apart'} sx={{height: "60px", padding: "0 10px"}} noWrap>
 				<MainSearchBar width="100%"/>
@@ -14,11 +26,17 @@ export default function VodViewHeader() {
 		</MediaQuery>
 
 		<MediaQuery smallerThan={'sm'} styles={{display: "none"}}>
-			<Group align={'center'} position={'apart'} sx={{height: "60px", padding: "0 10px"}} noWrap>
+			<Group align={'end'} position={'apart'} sx={{height: "60px", padding: "0 10px"}} noWrap>
 				<Link href="/">
 					<Button component="a" leftIcon={<ChevronLeft/>}>Back</Button>
 				</Link>
-				<MainSearchBar width="30%"/>
+				<Group noWrap>
+					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('language', value ?? '')} value={props.searchProperties.language} label='Language' data={languages} searchable/>
+					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('sortBy', value ?? '')} value={props.searchProperties.sortBy} label='Sort by' data={[{ value: 'time', label: 'Latest'}, { value: 'views', label: 'Views'}, { value: 'trending', label: 'Trending'}]}/>
+					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('period', value ?? '')} value={props.searchProperties.period} label='Period' data={[{ value: 'all', label: 'All'}, { value: 'day', label: 'Day'}, { value: 'week', label: 'Week'}, { value: 'month', label: 'Month'}]}/>
+					<Select sx={{ width: "150px"}} onChange={value => props.handleSearchPropertyChange('type', value ?? '')} value={props.searchProperties.type} label='VOD type' data={[{ value: 'all', label: 'All'}, { value: 'upload', label: 'Upload'}, { value: 'archive', label: 'Archive'}, { value: 'highlight', label: 'Highlight '}]}/>
+				</Group>
+				<MainSearchBar width="20%"/>
 				<DarkModeSwitch/>
 			</Group>
 		</MediaQuery>
--- pages/game/[gameName].tsx
+++ pages/game/[gameName].tsx
@@ -2,15 +2,29 @@ import { AspectRatio, Group, Title, Image, Space } from "@mantine/core";
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 import VodViewHeader from "../../components/VodViewHeader";
-import { languageCodes } from "../../data/languageCodes";
 
 export default function GameVodView() {
+	const [searchProperties, setSearchProperties] = useState({
+		language: 'en',
+		sortBy: 'time',
+		period: 'all',
+		type: 'all'
+	})
 	const router = useRouter();
 	const { gameName } = router.query;
 
+	function handleSearchPropertyChange(field: 'language' | 'sortBy' | 'period' | 'type', value: string) {
+		if (value) {
+			setSearchProperties(prevState => {
+				const newState = {...prevState }
+				newState[field] = value;
+				return newState
+			})
+		}
+	}
 
 	return <div>
-		<VodViewHeader gameName={gameName as string}/>
+		<VodViewHeader searchProperties={searchProperties} handleSearchPropertyChange={handleSearchPropertyChange}/>
 		<Space h={"md"}/>
 		<Group position="center" align={"self-end"}>
 		</Group>
