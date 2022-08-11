fd006b59c5590d0b1741378da429aa64cd903f97
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-11 20:01:32 +0100

    fix api resolving without response

--- pages/api/vods/[gameName].ts
+++ pages/api/vods/[gameName].ts
@@ -4,25 +4,22 @@ import { Period, searchParameters, SortBy, VodType } from '../../../lib/interfac
 import { getGameVods, getGame } from '../../../lib/twitch';
 
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-	(async () => {
-		const { gameName, language, sortBy, period, vodType } = req.query;
-		const game = await getGame(gameName as string);
-		const searchParameters: searchParameters = {
-			language: language as string,
-			sortBy: sortBy as SortBy,
-			period: period as Period,
-			vodType: vodType as VodType
-		}
-
-		if (game) {
-			const vods = await getGameVods(game, searchParameters)
-			res.status(200).json(vods.data.map(vod => vod[rawDataSymbol]));
-		} else {
-			res.status(400).send("Invalid Parameters")
-		}
-	})();
+	const { gameName, language, sortBy, period, vodType } = req.query;
+	const game = await getGame(gameName as string);
+	const searchParameters: searchParameters = {
+		language: language as string,
+		sortBy: sortBy as SortBy,
+		period: period as Period,
+		vodType: vodType as VodType
+	}
+	if (game) {
+		const vods = await getGameVods(game, searchParameters)
+		res.status(200).json(vods.data.map(vod => vod[rawDataSymbol]));
+	} else {
+		res.status(400).send("Invalid Parameters")
+	}
 }
