b563549cdad155c69731c340a968b81fa34828cb
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-11 17:46:33 +0100

    fetch vod data on page load

--- pages/game/[gameName].tsx
+++ pages/game/[gameName].tsx
@@ -1,25 +1,46 @@
-import { AspectRatio, Group, Title, Image, Space } from "@mantine/core";
+import { AspectRatio, Group, Title, Image, Space, Text } from "@mantine/core";
+import { HelixVideo } from "@twurple/api/lib";
+import { rawDataSymbol } from "@twurple/common";
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 import VodViewHeader from "../../components/VodViewHeader";
-import { searchProperties } from "../../lib/interfaces";
+import { searchParameters } from "../../lib/interfaces";
 
 export default function GameVodView() {
-	const [searchProperties, setSearchProperties] = useState<searchProperties>({
+	const [searchParameters, setsearchParameters] = useState<searchParameters>({
 		language: 'en',
 		sortBy: 'time',
 		period: 'all',
 		vodType: 'all'
 	})
+	const [vodResults, setVodResults] = useState<HelixVideo[]>([])
 	const router = useRouter();
 	const { gameName } = router.query;
 
-	
+	async function getVods(gameName: string, searchParameters: searchParameters) {
+		const vods = await fetch(`/api/vods/${gameName}?` + new URLSearchParams({
+			language: searchParameters.language,
+			sortBy: searchParameters.sortBy,
+			period: searchParameters.period,
+			vodType: searchParameters.vodType
+		}))
+		const vodData = await vods.json()
+		setVodResults(vodData);
+	}
+
+	useEffect(() => {
+		if (router.isReady) {
+			getVods(gameName as string, searchParameters)
+		}
+	}, [router.isReady, gameName, searchParameters])
 
 	return <div>
-		<VodViewHeader searchProperties={searchProperties} setSearchProperties={setSearchProperties}/>
+		<VodViewHeader searchParameters={searchParameters} setsearchParameters={setsearchParameters}/>
 		<Space h={"md"}/>
-		<Group position="center" align={"self-end"}>
-		</Group>
+		{
+			vodResults.map((vod, index) => {
+				return <Text key={index}>{vod.title}</Text>
+			})
+		}
 	</div>
 }
