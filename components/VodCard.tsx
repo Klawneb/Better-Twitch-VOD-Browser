bb0554d6f6b50395311bfa68a82d39bc34483172
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-13 00:21:22 +0100

    display vod thumbnails properly

--- components/VodCard.tsx
+++ components/VodCard.tsx
@@ -1,15 +1,28 @@
-import { Card, Image, Text } from "@mantine/core";
+import { Card, Image, Text, AspectRatio } from "@mantine/core";
 import { HelixVideo } from "@twurple/api";
+import { HelixVideoData } from "@twurple/api/lib/api/helix/video/HelixVideo";
 
 interface VodCardProps {
-	vod: HelixVideo
+	vod: HelixVideoData
 }
 
 export default function VodCard({ vod }: VodCardProps) {
+	function formatThumbnailURL(thumbnailUrl: string, width: number, height: number) {
+		let newURL = thumbnailUrl.replace('%{width}', width.toString());
+		newURL = newURL.replace('%{height}', height.toString());
+		return newURL;
+	}
 	
-	return <Card>
+	return (
+			<Card>
 		<Card.Section>
-			<Image src={vod.thumbnailUrl} withPlaceholder/>
+			<AspectRatio ratio={16/9}>
+				<Image src={formatThumbnailURL(vod.thumbnail_url, 480, 360)} withPlaceholder alt={'VOD Thumbnail'}/>
+			</AspectRatio>
 		</Card.Section>
-	</Card>
+
+		<Card.Section>
+			<Text>{vod.title}</Text>
+		</Card.Section>
+	</Card>)
 }
