a99ec12adfa87f482a1715145247aac5cd5e4eb4
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-16 23:58:40 +0100

    fix placeholder rendering over thumbnail sometimes

--- components/VodCard.tsx
+++ components/VodCard.tsx
@@ -1,7 +1,7 @@
 import { Card, Image, Text, AspectRatio, Group, Tooltip, Overlay, Space, Anchor, useMantineColorScheme } from "@mantine/core";
 import { HelixVideoData } from "@twurple/api/lib/api/helix/video/HelixVideo";
 import { format } from "date-fns";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface VodCardProps {
 	vod: HelixVideoData
@@ -9,6 +9,7 @@ interface VodCardProps {
 
 export default function VodCard({ vod }: VodCardProps) {
 	const [overlayVisible, setOverlayVisible] = useState(false);
+	const [thumbnailUrl, setThumbnailUrl] = useState("")
 	const { colorScheme } = useMantineColorScheme()
 
 	function formatThumbnailURL(thumbnailUrl: string, width: number, height: number) {
@@ -17,27 +18,36 @@ export default function VodCard({ vod }: VodCardProps) {
 		return newURL;
 	}
 
-	return (<Card shadow={'lg'}>
+	useEffect(() => {
+		setThumbnailUrl(formatThumbnailURL(vod.thumbnail_url, 480, 360))
+	}, [vod.thumbnail_url])
+
+	return (<Card shadow={'lg'} radius={10} p={0}>
 		<Card.Section sx={{backgroundColor: colorScheme === 'dark' ? '#141517' : '#f8f9fa'}}>
 			<AspectRatio ratio={16/9}>
 				<Anchor href={`${vod.url}`}>
-					<Image src={formatThumbnailURL(vod.thumbnail_url, 480, 360)} withPlaceholder alt="No thumbnail found"/>
+					{
+						thumbnailUrl ?
+						<Image src={thumbnailUrl} alt="No thumbnail found"/>
+						:
+						<Image withPlaceholder alt="No thumbnail found"/>
+					}
 				</Anchor>
-				{overlayVisible && <Text sx={{zIndex: 2}} p={50} size={24}>{vod.title}</Text>}
+				{overlayVisible && <Text sx={{zIndex: 2}} p={50} size={24} color={'white'}>{vod.title}</Text>}
 				{overlayVisible && <Overlay opacity={0.6} blur={2} color="#000" zIndex={0}>hello</Overlay>}
 			</AspectRatio>
 		</Card.Section>
 
-		<Card.Section>
+		<Card.Section px={5}>
 			<Group position="apart" noWrap>
 				<Text sx={{cursor: 'pointer'}} lineClamp={1} onMouseEnter={() => setOverlayVisible(true)} onMouseLeave={() => setOverlayVisible(false)}>{vod.title}</Text>
 				<Text sx={{minWidth: "100px"}} align="end">{format(new Date(vod.published_at), "do-MMM-yy")}</Text>
 			</Group>
 			<Space h={"xs"}/>
 			<Group noWrap position="apart">
-				<Text>{vod.view_count} views</Text>
+				<Text lineClamp={1}>{vod.view_count} views</Text>
 				<Anchor href={`https://twitch.tv/${vod.user_name}`}>{vod.user_name}</Anchor>
-				<Text>{vod.duration}</Text>
+				<Text lineClamp={1}>{vod.duration}</Text>
 			</Group>
 			<Text></Text>
 		</Card.Section>
