9238fc3c3cd64287c90c4bc19be537f3fbdcf555
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-17 17:01:08 +0100

    consolidate pagination functions into one

--- pages/game/[gameName].tsx
+++ pages/game/[gameName].tsx
@@ -15,51 +15,25 @@ export default function GameVodView() {
 		period: 'all',
 		vodType: 'all'
 	})
-	const { colorScheme } = useMantineColorScheme()
 	const [vodResults, setVodResults] = useState<HelixVideoData[]>([])
 	const [paginationToken, setPaginationToken] = useState('');
+	const [pageNo, setPageNo] = useState(1);
 	const [isLoading, setIsLoading] = useState(false);
+
 	const router = useRouter();
 	const { gameName } = router.query;
-	const [pageNo, setPageNo] = useState(1);
 
-	async function getVods(gameName: string, searchParameters: searchParameters) {
-		setIsLoading(true);
-		const vods = await fetch(`/api/vods/${gameName}?` + new URLSearchParams({
-			language: searchParameters.language,
-			sortBy: searchParameters.sortBy,
-			period: searchParameters.period,
-			vodType: searchParameters.vodType
-		}))
-		const vodData: HelixPaginatedResult<HelixVideoData> = await vods.json()
-		setVodResults(vodData.data);
-		setPaginationToken(vodData.cursor ?? '');
-		setIsLoading(false);
-	}
+	const { colorScheme } = useMantineColorScheme()
 
-	async function getVodsAfter(gameName: string, searchParameters: searchParameters, paginationToken: string) {
+	async function getVods(gameName: string, searchParameters: searchParameters, after: string = '', before: string = '') {
 		setIsLoading(true);
 		const vods = await fetch(`/api/vods/${gameName}?` + new URLSearchParams({
 			language: searchParameters.language,
 			sortBy: searchParameters.sortBy,
 			period: searchParameters.period,
 			vodType: searchParameters.vodType,
-			after: paginationToken
-		}))
-		const vodData: HelixPaginatedResult<HelixVideoData> = await vods.json()
-		setVodResults(vodData.data);
-		setPaginationToken(vodData.cursor ?? '');
-		setIsLoading(false);
-	}
-
-	async function getVodsBefore(gameName: string, searchParameters: searchParameters, paginationToken: string) {
-		setIsLoading(true);
-		const vods = await fetch(`/api/vods/${gameName}?` + new URLSearchParams({
-			language: searchParameters.language,
-			sortBy: searchParameters.sortBy,
-			period: searchParameters.period,
-			vodType: searchParameters.vodType,
-			before: paginationToken
+			after: after,
+			before: before
 		}))
 		const vodData: HelixPaginatedResult<HelixVideoData> = await vods.json()
 		setVodResults(vodData.data);
@@ -75,7 +49,7 @@ export default function GameVodView() {
 			getVods(gameName as string, searchParameters)
 		}
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [router.isReady, gameName, searchParameters])
+	}, [router.isReady, searchParameters])
 
 	return <Stack sx={{height: "100vh"}} spacing={0}>
 		<VodViewHeader searchParameters={searchParameters} setsearchParameters={setsearchParameters}/>
@@ -91,9 +65,15 @@ export default function GameVodView() {
 			<Stack style={{width: "75%", margin: "0 auto"}}>
 				<VodView vodList={vodResults}/>
 				<Group position="apart">
-					<Button onClick={() => {getVodsBefore(gameName as string, searchParameters, paginationToken); setPageNo(prevState => prevState - 1)}}>Previous Page</Button>
+					<Button disabled={pageNo === 1} onClick={() => {
+						getVods(gameName as string, searchParameters, '', paginationToken); 
+						setPageNo(prevState => prevState - 1)
+					}}>Previous Page</Button>
 					<Text>Page {pageNo}</Text>
-					<Button onClick={() => {getVodsAfter(gameName as string, searchParameters, paginationToken); setPageNo(prevState => prevState + 1)}}>Next Page</Button>
+					<Button onClick={() => {
+						getVods(gameName as string, searchParameters, paginationToken);
+						setPageNo(prevState => prevState + 1)}
+					}>Next Page</Button>
 				</Group>
 			</Stack>
 			:
