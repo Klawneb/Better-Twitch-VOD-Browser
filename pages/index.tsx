2af3727ad6ebcd72d7ed122f5360c99eda2005c5
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-27 18:46:16 +0100

    adapt homepage to login state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,32 @@
-import { Center, Stack, Title, Text, Switch, useMantineColorScheme} from '@mantine/core'
+import { Center, Stack, Title, Text, Switch, useMantineColorScheme, Group, Anchor} from '@mantine/core'
 import type { NextPage } from 'next'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import DarkModeSwitch from '../components/DarkModeSwitch'
 import MainSearchBar from '../components/MainSeachBar'
 
 function Home() {
+	const session = useSession();
+
 	return (
 		<Center sx={{ height: "100vh"}}>
 			<Stack align='center'>
 				<Title>Better Twitch VOD Browser</Title>
 				<MainSearchBar width='125%'/>
-				<Text align='center'><Text onClick={() => signIn()} underline component='span'>Login with Twitch</Text> to get followed streamer VODs</Text>
+				{
+					session ?
+					<Group position='apart' grow sx={{width: "125%"}}>
+						<Text  align="center">Signed in as {session.data?.user?.name}</Text>
+						<Link href={"/followed"}>
+							<Anchor component='a' align='center'>Followed VODs</Anchor>
+						</Link>
+						<Text variant='link' align='center' sx={{cursor: "pointer"}}>Sign Out</Text>
+					</Group>
+					:
+					<Text align='center'><Text onClick={() => signIn()} underline component='span'>Login with Twitch</Text> to get followed streamer VODs</Text>
+				}
 				<DarkModeSwitch/>
 			</Stack>
 		</Center>
