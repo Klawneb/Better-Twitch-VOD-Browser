6c673d800513fd5a15a986f23ecf29a3328c6a4a
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-29 19:46:13 +0100

    add limited vods endpoint

---
+++ pages/api/vods/user/[userName]/[limit]/index.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getUser, getUserVodsLimited } from '../../../../../../lib/twitch';
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+  ) {
+	  const { userName, limit} = req.query;
+	  const user = await getUser(userName as string);
+  
+	  if (user) {
+		  let vods = await getUserVodsLimited(user, Number(limit as string));
+		  res.status(200).json(
+			  vods
+		  );
+	  } 
+	  else {
+		  res.status(400).send("Invalid Parameters")
+	  }
+  }
