670bc85ec0bf4cbf70a907b15ee159dbf7ab1afc
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-30 19:14:02 +0100

    add vod fetching and display

--- pages/followed.tsx
+++ pages/followed.tsx
@@ -1,11 +1,14 @@
-import { Stack, Title, Text, Center } from "@mantine/core";
+import { Stack, Title, Text, Center, Button } from "@mantine/core";
+import { HelixVideoData } from "@twurple/api/lib/api/helix/video/HelixVideo";
 import { signIn, useSession } from "next-auth/react"
 import { useEffect, useState } from "react";
+import VodView from "../components/VodView";
 import VodViewHeader from "../components/VodViewHeader";
 
 export default function Followed() {
 	const session = useSession();
-	const [followedUsers, setFollowedUsers] = useState<string[]>([])
+	const [followedUsers, setFollowedUsers] = useState<string[]>([]);
+    const [followedVods, setFollowedVods] = useState<HelixVideoData[]>([]);
 
 	useEffect(() => {
 		if (session.status === 'authenticated') {
@@ -16,12 +19,22 @@ export default function Followed() {
 				setFollowedUsers(await followed.json());
 			})();
 		}
-	}, [])
+	}, [session.status, session.data?.user?.name])
+
+    useEffect(() => {
+        followedUsers.forEach(async username => {
+            let vods: HelixVideoData[] = await (await fetch(`/api/vods/user/${username}/10`)).json();
+            setFollowedVods(prevState => [...prevState, ...vods]);
+        })
+    }, [followedUsers])
 
 	return (
 		session.status === 'authenticated' ?
 		<Stack>
 			<VodViewHeader pageTitle={"Followed VODs"}/>
+            <VodView vodList={followedVods.sort((a, b) => {
+                return new Date(b.published_at).getTime() - new Date(a.published_at).getTime();
+            })}/>
 		</Stack>
 		:
 		<Center sx={{height: "100vh"}}>
