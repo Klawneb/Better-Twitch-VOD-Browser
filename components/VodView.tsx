8fb0c8a8564a680837fd2ec5d2a71e5362510f2a
Benjamin Walker <benjaminwalker222@gmail.com>
2022-08-30 22:06:12 +0100

    use stack and restrict width

--- components/VodView.tsx
+++ components/VodView.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, SimpleGrid, Text, useMantineColorScheme } from "@mantine/core"
+import { Button, Group, SimpleGrid, Stack, Text, useMantineColorScheme } from "@mantine/core"
 import { HelixVideo } from "@twurple/api"
 import { HelixVideoData } from "@twurple/api/lib/api/helix/video/HelixVideo"
 import { useEffect, useState } from "react"
@@ -17,27 +17,27 @@ export default function VodView(props: VodViewProps) {
 	},[pageNo, props.vodList])
 
 	return (
-	<>
-	<SimpleGrid cols={5}
-	  breakpoints={[
-        { maxWidth: 1600, cols: 4, spacing: 'md' },
-		{ maxWidth: 1200, cols: 2, spacing: 'sm' },
-        { maxWidth: 800, cols: 2, spacing: 'sm' },
-      ]}>
-		{
-			currentPage.map((video, index) => {
-				return <VodCard key={index} vod={video}/>
-			})
-		}
-	</SimpleGrid>
-	<Group position="apart">
-					<Button disabled={pageNo === 1} onClick={() => {
-						setPageNo(prevState => prevState - 1);
-					}}>Previous Page</Button>
-					<Text>Page {pageNo}</Text>
-					<Button onClick={() => {
-						setPageNo(prevState => prevState + 1);
-					}}>Next Page</Button>
-	</Group>
-	</>)
+	<Stack sx={{width: "75%", margin: '0 auto'}}>
+		<SimpleGrid cols={5}  
+		breakpoints={[
+			{ maxWidth: 1600, cols: 4, spacing: 'md' },
+			{ maxWidth: 1200, cols: 2, spacing: 'sm' },
+			{ maxWidth: 800, cols: 2, spacing: 'sm' },
+		]}>
+			{
+				currentPage.map((video, index) => {
+					return <VodCard key={index} vod={video}/>
+				})
+			}
+		</SimpleGrid>
+		<Group position="apart">
+						<Button disabled={pageNo === 1} onClick={() => {
+							setPageNo(prevState => prevState - 1);
+						}}>Previous Page</Button>
+						<Text>Page {pageNo}</Text>
+						<Button onClick={() => {
+							setPageNo(prevState => prevState + 1);
+						}}>Next Page</Button>
+		</Group>
+	</Stack>)
 }
